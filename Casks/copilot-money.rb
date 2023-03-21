2839f0edb440953843d5119e4d96b332a707bd61
Sergio Campamá <kaipi@copilot.money>
2023-03-21 15:57:47 -0300

    Add Copilot.app v3.7.7b77

---
+++ Casks/copilot-money.rb
@@ -0,0 +1,11 @@
+cask "copilot-money" do
+  version "3.1.1-77"
+  sha256 "76c19a992568a7948fd999c63d6a4e53b40f9765dde0508807ae5c26bb27adbd"
+
+  url "https://copilot.money/releases/Copilot-#{version}-76c19a99.dmg"
+  name "copilot-money"
+  desc "Do money better with Copilot Money"
+  homepage "https://copilot.money/"
+
+  app "Copilot.app"
+end
